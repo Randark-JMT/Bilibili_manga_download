8525ad17a29c28a88baba7a37b08082efb2fef65
randark <randark@foxmail.com>
2021-07-25 21:33:44 +0800

    优化日志输出

--- download_gui.py
+++ download_gui.py
@@ -7,21 +7,20 @@ from settings import download_path, headers
 from tkinter.scrolledtext import ScrolledText
 
 
-def download_gui(id_input: int, download_range: str, sessdata: str, text_output: ScrolledText):
+# 主下载模块
+def download_gui(comic_id: int, download_range: str, sessdata: str, text_output: ScrolledText):
     # cookie 数据读取
     sessdata = 'SESSDATA=' + sessdata
     headers['cookie'] = sessdata
-    download_manga_title_gui(id_input, text_output)
+    download_manga_title_gui(comic_id, text_output)
 
     if download_range == '0':
-        download_manga_all_gui(id_input, text_output)
+        download_manga_all_gui(comic_id, text_output)
         exit(0)
     # 防呆设计
     download_range.replace('，', ',')
-    # download_range.replace('-', '-')
-    text_output.insert("insert", download_range + '\n')
-    text_output.see('insert')
-    text_output.update()
+    download_range.replace('—', '-')
+    # main_gui_log('log_normal', download_range + '\n', text_output)
 
     # 对用户输入的章节数据进行读取
     start = int(0)  # 范围下载第一位
@@ -33,14 +32,10 @@ def download_gui(id_input: int, download_range: str, sessdata: str, text_output:
         if letter == ',':
             if not state:
                 section = start
-                download_manga_each_gui(id_input, section, text_output)
+                download_manga_each_gui(comic_id, section, text_output)
                 continue
             else:
-                section = start
-                while section <= destination:
-                    # print(section)
-                    download_manga_each_gui(id_input, section, text_output)
-                    section += 1
+                download_manga_range_gui(comic_id, start, destination, text_output)
                 # 初始化控制变量
                 start = int(0)
                 destination = int(0)
@@ -55,21 +50,22 @@ def download_gui(id_input: int, download_range: str, sessdata: str, text_output:
             if state:
                 destination = destination * 10 + int(letter)
                 continue
-    # TODO 将范围下载的函数独立出来
     # 检查是否为最后一块
     if frequency == len(download_range):
         if not state:
             section = start
-            download_manga_each_gui(id_input, section, text_output)
+            download_manga_each_gui(comic_id, section, text_output)
         else:
-            section = start
-            while section <= destination:
-                # print(section)
-                download_manga_each_gui(id_input, section, text_output)
-                section = section + 1
-    text_output.insert("insert", '下载完毕' + '\n')
-    text_output.see('insert')
-    text_output.update()
+            download_manga_range_gui(comic_id, start, destination, text_output)
+    main_gui_log_insert('下载完毕' + '\n\n\n', text_output)
+
+
+# 范围下载漫画模块
+def download_manga_range_gui(comic_id: int, start: int, destination: int, text_output: ScrolledText):
+    section: int = start
+    while section <= destination:
+        download_manga_each_gui(comic_id, section, text_output)
+        section += 1
 
 
 # 全部漫画下载模块
@@ -88,14 +84,11 @@ def download_manga_all_gui(comic_id: int, text_output: ScrolledText):
     for ep in manga_list:
         # 检查付费章节是否购买
         if not ep['is_locked']:
-            # text_output.insert('insert', 'test\n')
-            text_output.insert("insert", '正在下载第:' + ep['short_title'] + ep['title'] + '\n')
-            text_output.see('insert')
-            text_output.update()
+            main_gui_log_insert('正在下载第:' + ep['short_title'] + ep['title'] + '\n', text_output)
             download_manga_episode_gui(ep['id'], root_path, text_output)
 
 
-# 逐章节下载模块
+# 单章节下载模块
 def download_manga_each_gui(comic_id: int, section: int, text_output: ScrolledText):
     url = str("https://manga.bilibili.com/twirp/comic.v2.Comic/ComicDetail?device=pc&platform=web")
     res = requests.post(url, json.dumps({"comic_id": comic_id}), headers=headers)
@@ -112,11 +105,8 @@ def download_manga_each_gui(comic_id: int, section: int, text_output: ScrolledTe
         if not ep['is_locked']:
             # section_temp = int(ep['short_title'])
             if int(ep['short_title']) == section:
-                # print('正在下载章节:', ep['short_title'], ep['title'])
-                text_output.insert("insert", '正在下载第' + ep['short_title'].rjust(3, '0') + '话：' + ep['title'] + '\n')
-                text_output.see('insert')
-                text_output.update()
-                download_manga_episode_gui(ep['id'], root_path, text_output)
+                main_gui_log_insert('正在下载第' + ep['short_title'].rjust(3, '0') + '话：' + ep['title'] + '\n', text_output)
+                # download_manga_episode_gui(ep['id'], root_path, text_output)
 
 
 # ID~索引下载漫画模块
@@ -129,9 +119,7 @@ def download_manga_episode_gui(episode_id: int, root_path: str, text_output: Scr
     # title = comic_title + '_' + short_title + '_' + data['data']['title']
     title = short_title + '_' + data['data']['title']
     comic_id = data['data']['comic_id']
-    # text_output.insert("insert", '正在下载：' + title + '\n')
-    # text_output.see('insert')
-    # text_output.update()
+    main_gui_log_insert('正在下载：' + title + '\n', text_output)
 
     # 获取索引文件cdn位置
     url = str('https://manga.bilibili.com/twirp/comic.v1.Comic/GetImageIndex?device=pc&platform=web')
@@ -144,14 +132,13 @@ def download_manga_episode_gui(episode_id: int, root_path: str, text_output: Scr
     # 解析索引文件
     pics = decode_index_data(comic_id, episode_id, res.content)
     # print(pics)
+    # 文件储存
     ep_path = os.path.join(root_path, title)
     if not os.path.exists(ep_path):
         os.makedirs(ep_path)
     for i, e in enumerate(pics):
         url = get_image_url(e)
-        text_output.insert("insert", '第' + str(i + 1) + '页    ' + e + '\n')
-        text_output.see('insert')
-        text_output.update()
+        main_gui_log_insert('第' + str(i + 1) + '页    ' + e + '\n', text_output)
         res = requests.get(url)
         with open(os.path.join(ep_path, str(i + 1).rjust(3, '0') + '.jpg'), 'wb+') as f:
             f.write(res.content)
@@ -160,11 +147,17 @@ def download_manga_episode_gui(episode_id: int, root_path: str, text_output: Scr
             time.sleep(2)
 
 
+# 漫画标题获取
 def download_manga_title_gui(comic_id: int, text_output: ScrolledText):
     url = str("https://manga.bilibili.com/twirp/comic.v2.Comic/ComicDetail?device=pc&platform=web")
     res = requests.post(url, json.dumps({"comic_id": comic_id}), headers=headers)
     data = json.loads(res.text)['data']
     comic_title = data['title']
-    text_output.insert("insert", '正在下载的漫画为：' + comic_title + '\n')
+    main_gui_log_insert('正在下载的漫画为：' + comic_title + '\n', text_output)
+
+
+# 日志输出(带刷新界面)
+def main_gui_log_insert(msg: str, text_output: ScrolledText):
+    text_output.insert('insert', msg)
     text_output.see('insert')
     text_output.update()

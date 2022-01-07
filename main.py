2eebacf67efd0f14895d58f7355a12d4bc109300
Randark_JMT <randark@foxmail.com>
2022-01-07 20:49:45 +0800

    重构界面，等待填充行为

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
-import ctypes
-from gui import MainGUI
-from tkinter.tix import Tk
-from settings import cookie_file, download_path
 import sys
-import os
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import QFile
+from ui_MainGUI_Pyside6 import Ui_MainWindow
 
-if __name__ == '__main__':
-    if hasattr(sys, 'frozen'):
-        os.environ['PATH'] = sys._MEIPASS + ";" + os.environ['PATH']
-    ctypes.windll.shcore.SetProcessDpiAwareness(1)
-    ScaleFactor = ctypes.windll.shcore.GetScaleFactorForDevice(0)
-    if not os.path.exists(download_path):
-        os.makedirs(download_path)
-    if not os.path.exists(cookie_file):
-        file = open(cookie_file, 'w')
-        file.close()
-    manga_window = Tk()
-    manga_window.tk.call('tk', 'scaling', ScaleFactor / 75)
-    manga_window.wm_geometry('900x600')
-    manga_window.wm_resizable(False, False)
-    manga_window.title('Bilibili漫画下载    V1.2.2    仅限Mox内部使用')
-    manga_window.update()
-    MainGUI(manga_window).mainloop()
-    MainGUI(manga_window).destroy()
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
---
+++ ui_MainGUI_Pyside6.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'MainGUI_Pyside6.ui'
+##
+## Created by: Qt User Interface Compiler version 6.2.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFormLayout, QHeaderView, QLabel,
+    QListView, QMainWindow, QPlainTextEdit, QPushButton,
+    QSizePolicy, QStatusBar, QTabWidget, QTreeView,
+    QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(802, 598)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.formLayoutWidget = QWidget(self.centralwidget)
+        self.formLayoutWidget.setObjectName(u"formLayoutWidget")
+        self.formLayoutWidget.setGeometry(QRect(10, 10, 421, 121))
+        self.formLayout = QFormLayout(self.formLayoutWidget)
+        self.formLayout.setObjectName(u"formLayout")
+        self.formLayout.setContentsMargins(0, 0, 0, 0)
+        self.label = QLabel(self.formLayoutWidget)
+        self.label.setObjectName(u"label")
+
+        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label)
+
+        self.label_2 = QLabel(self.formLayoutWidget)
+        self.label_2.setObjectName(u"label_2")
+
+        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.label_2)
+
+        self.label_3 = QLabel(self.formLayoutWidget)
+        self.label_3.setObjectName(u"label_3")
+
+        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.label_3)
+
+        self.plainTextEdit = QPlainTextEdit(self.formLayoutWidget)
+        self.plainTextEdit.setObjectName(u"plainTextEdit")
+
+        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.plainTextEdit)
+
+        self.plainTextEdit_2 = QPlainTextEdit(self.formLayoutWidget)
+        self.plainTextEdit_2.setObjectName(u"plainTextEdit_2")
+
+        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.plainTextEdit_2)
+
+        self.plainTextEdit_3 = QPlainTextEdit(self.formLayoutWidget)
+        self.plainTextEdit_3.setObjectName(u"plainTextEdit_3")
+
+        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.plainTextEdit_3)
+
+        self.tabWidget = QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tabWidget.setGeometry(QRect(10, 150, 781, 431))
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.treeView = QTreeView(self.tab)
+        self.treeView.setObjectName(u"treeView")
+        self.treeView.setGeometry(QRect(20, 20, 741, 361))
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.listView = QListView(self.tab_2)
+        self.listView.setObjectName(u"listView")
+        self.listView.setGeometry(QRect(10, 20, 761, 371))
+        self.tabWidget.addTab(self.tab_2, "")
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(620, 20, 141, 41))
+        self.pushButton_2 = QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(450, 20, 161, 41))
+        self.pushButton_3 = QPushButton(self.centralwidget)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton_3.setGeometry(QRect(450, 90, 151, 41))
+        self.pushButton_4 = QPushButton(self.centralwidget)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+        self.pushButton_4.setGeometry(QRect(630, 80, 121, 41))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        self.tabWidget.setCurrentIndex(0)
+
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Bilibili\u6f2b\u753b\u4e0b\u8f7d\u5668  V1.4.0", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"\u7528\u6237Cookie\uff1a", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u6f2b\u753bID\uff1a", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"\u4e0b\u8f7d\u8303\u56f4\uff1a", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("MainWindow", u"\u6f2b\u753b\u4fe1\u606f", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("MainWindow", u"\u8f93\u51fa\u65e5\u5fd7", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\u4fdd\u5b58Cookie", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"\u67e5\u8be2\u8d2d\u4e70\u60c5\u51b5", None))
+        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"\u5f00\u59cb\u4e0b\u8f7d", None))
+        self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"\u4e2d\u65ad", None))
+    # retranslateUi
+

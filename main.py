4e797fc65fdd6f90df57a70a0c4fb3d37846cd3b
Randark_JMT <randark@foxmail.com>
2022-01-07 21:41:18 +0800

    成功研究函数与GUI的连接方案

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
-from PySide6.QtCore import QFile
+from PySide6.QtCore import QFile, Slot
 from ui_MainGUI_Pyside6 import Ui_MainWindow
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+    def hello(self):
+        self.label_3.setText("test")
 
 
 if __name__ == "__main__":

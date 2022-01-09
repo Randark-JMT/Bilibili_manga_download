efe0d9e2da21a9bd9af85c7c18d2c07bdc514fce
Randark_JMT <randark@foxmail.com>
2022-01-09 23:38:10 +0800

    优化界面卡死的解决方案

--- main.py
+++ main.py
@@ -19,22 +19,25 @@ def check_datafile():
 class MainWindow(QMainWindow, Ui_MainWindow):
     @Slot()
     def check_purchase_staus(self):
+        QMessageBox.warning(self, "提示", "注意，此操作需要一定耗时，请耐心等待，不要随便关闭窗口\n开发者正在尝试解决此问题，请谅解。")
         from downloader import download_purchase_status
         data_rt = download_purchase_status(self.textEdit_3.toPlainText(), self.textEdit.toPlainText())
         root = QTreeWidgetItem(self.treeWidget)
-        root.setText(0, str(data_rt[0][0]) + "  " + data_rt[0][1])
+        root.setText(0, "查询  " + str(data_rt[0][0]) + "  " + data_rt[0][1])
         root.setText(1, "查询时间：" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
         for data in data_rt[1:]:
             child = QTreeWidgetItem(root)
             child.setText(0, data[0])
             child.setText(1, data[1])
+        self.treeWidget.expandItem(root)
+        self.treeWidget.scrollToBottom()
 
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
         # 漫画信息窗口-初始化
         self.treeWidget.setColumnCount(2)
-        self.treeWidget.setHeaderLabels(['Key', 'Value'])
+        self.treeWidget.setHeaderLabels(['字段', '数据'])
         self.treeWidget.setColumnWidth(0, 400)
         self.pushButton_2.clicked.connect(self.check_purchase_staus)
 
--- ui_MainGUI_Pyside6.py
+++ ui_MainGUI_Pyside6.py
@@ -15,21 +15,20 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFormLayout, QFrame, QHeaderView,
-    QLabel, QMainWindow, QPushButton, QSizePolicy,
-    QStatusBar, QTabWidget, QTextBrowser, QTextEdit,
-    QTreeWidget, QTreeWidgetItem, QWidget)
+from PySide6.QtWidgets import (QApplication, QFormLayout, QHeaderView, QLabel,
+    QMainWindow, QPushButton, QSizePolicy, QStatusBar,
+    QTextEdit, QTreeWidget, QTreeWidgetItem, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(848, 667)
+        MainWindow.resize(774, 625)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.formLayoutWidget = QWidget(self.centralwidget)
         self.formLayoutWidget.setObjectName(u"formLayoutWidget")
-        self.formLayoutWidget.setGeometry(QRect(10, 10, 421, 131))
+        self.formLayoutWidget.setGeometry(QRect(10, 10, 411, 121))
         self.formLayout = QFormLayout(self.formLayoutWidget)
         self.formLayout.setObjectName(u"formLayout")
         self.formLayout.setContentsMargins(0, 0, 0, 0)
@@ -63,47 +62,30 @@ class Ui_MainWindow(object):
 
         self.formLayout.setWidget(1, QFormLayout.FieldRole, self.textEdit_3)
 
-        self.tabWidget = QTabWidget(self.centralwidget)
-        self.tabWidget.setObjectName(u"tabWidget")
-        self.tabWidget.setGeometry(QRect(10, 150, 791, 451))
-        self.tabWidget.setIconSize(QSize(8, 8))
-        self.tab = QWidget()
-        self.tab.setObjectName(u"tab")
-        self.treeWidget = QTreeWidget(self.tab)
-        self.treeWidget.setObjectName(u"treeWidget")
-        self.treeWidget.setGeometry(QRect(20, 10, 751, 401))
-        self.treeWidget.setContextMenuPolicy(Qt.DefaultContextMenu)
-        self.treeWidget.setStyleSheet(u"")
-        self.treeWidget.setFrameShape(QFrame.StyledPanel)
-        self.treeWidget.setFrameShadow(QFrame.Sunken)
-        self.tabWidget.addTab(self.tab, "")
-        self.tab_2 = QWidget()
-        self.tab_2.setObjectName(u"tab_2")
-        self.textBrowser = QTextBrowser(self.tab_2)
-        self.textBrowser.setObjectName(u"textBrowser")
-        self.textBrowser.setGeometry(QRect(10, 20, 761, 371))
-        self.tabWidget.addTab(self.tab_2, "")
         self.pushButton = QPushButton(self.centralwidget)
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(620, 20, 141, 41))
+        self.pushButton.setGeometry(QRect(610, 20, 141, 41))
         self.pushButton_2 = QPushButton(self.centralwidget)
         self.pushButton_2.setObjectName(u"pushButton_2")
-        self.pushButton_2.setGeometry(QRect(450, 20, 161, 41))
+        self.pushButton_2.setGeometry(QRect(440, 20, 161, 41))
         self.pushButton_3 = QPushButton(self.centralwidget)
         self.pushButton_3.setObjectName(u"pushButton_3")
-        self.pushButton_3.setGeometry(QRect(450, 90, 151, 41))
+        self.pushButton_3.setGeometry(QRect(440, 90, 151, 41))
         self.pushButton_4 = QPushButton(self.centralwidget)
         self.pushButton_4.setObjectName(u"pushButton_4")
-        self.pushButton_4.setGeometry(QRect(630, 80, 121, 41))
+        self.pushButton_4.setGeometry(QRect(620, 80, 121, 41))
+        self.treeWidget = QTreeWidget(self.centralwidget)
+        __qtreewidgetitem = QTreeWidgetItem()
+        __qtreewidgetitem.setText(0, u"1");
+        self.treeWidget.setHeaderItem(__qtreewidgetitem)
+        self.treeWidget.setObjectName(u"treeWidget")
+        self.treeWidget.setGeometry(QRect(10, 150, 751, 441))
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
-
-        self.tabWidget.setCurrentIndex(0)
-
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
@@ -116,8 +98,6 @@ class Ui_MainWindow(object):
 #if QT_CONFIG(whatsthis)
         self.textEdit.setWhatsThis(QCoreApplication.translate("MainWindow", u"\u6d4b\u8bd5", None))
 #endif // QT_CONFIG(whatsthis)
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("MainWindow", u"\u6f2b\u753b\u4fe1\u606f", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("MainWindow", u"\u8f93\u51fa\u65e5\u5fd7", None))
         self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\u4fdd\u5b58Cookie", None))
         self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"\u67e5\u8be2\u8d2d\u4e70\u60c5\u51b5", None))
         self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"\u5f00\u59cb\u4e0b\u8f7d", None))

525ec62d2c608e86ba9da8bdf664c76dbfdd0327
Randark_JMT <randark@foxmail.com>
2022-12-09 12:09:57 +0800

    初始化新分支

--- Bilibili/__init__.py
+++
@@ -1,2 +0,0 @@
-def download_bilibili(manga_id: int, manga_range: str):
-    pass
--- Bilibili/check.py
+++
@@ -1,6 +0,0 @@
-def check_manga_id(manga_id: int):
-    pass
-
-
-def check_manga_range(manga_range: str):
-    pass

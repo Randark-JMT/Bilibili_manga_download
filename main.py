262b685469e7a048cd096faacc1158819e0aba32
Randark_JMT <randark@foxmail.com>
2022-12-09 17:52:17 +0800

    初始化项目结构

--- main.py
+++ main.py
@@ -6,12 +6,9 @@ from resources.ui_MainGUI import Ui_MainWindow
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
-        self.thread_login = None
-        self.thread_download = None
         self.setupUi(self)
         version = "1.0.0"
         self.setWindowTitle("Bilibili漫画下载器  " + version)
-        # 漫画信息窗口-初始化
 
 
 if __name__ == "__main__":
